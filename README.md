d43eed4fe70f34eaffb73edba9dfbd0aa32f5558
Oussama Ben Nasr <oussama.bennasr@supcom.tn>
2024-03-02 13:45:22 +0100

    code: working out delta-table from the docs

---
+++ README.md
@@ -0,0 +1,20 @@
+# Application
+* Ingest csv files w/w.o headers
+* Append two colums to the output dataframe
+  * ingestion_tms (YYYY-MM-DD HH:mm:SS)
+  * use append mode to automatically append new data to delta table
+
+# Dockerize
+* spark job
+* spark history server
+
+# Deploy
+* locally kubernetes + minikube
+
+# docs
+[delta lake](https://docs.delta.io/1.2.1/quick-start.html)
+
+# versions
+* python: 3.10.12
+* pyspark: 3.3.1
+* delta: 1.2.1
---
+++ spark/app.py
@@ -0,0 +1,60 @@
+from delta import *
+from pyspark.sql.functions import *
+import pyspark
+from time import sleep
+
+builder = pyspark.sql.SparkSession.builder.appName("MyApp") \
+    .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+    .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog")
+
+spark = configure_spark_with_delta_pip(builder).getOrCreate()
+
+
+# Create table and save it
+data = spark.range(0, 5)
+data.write.format("delta").mode("overwrite").save("/tmp/delta-table")
+
+
+# Read table from local file system
+df = spark.read.format("delta").load("/tmp/delta-table")
+df.show()
+
+
+## Conditional update without overwrite
+deltaTable = DeltaTable.forPath(spark, "/tmp/delta-table")
+
+# Update every even value by adding 100 to it
+deltaTable.update(
+  condition = expr("id % 2 == 0"),
+  set = { "id": expr("id + 100") })
+
+# Delete every even value
+deltaTable.delete(condition = expr("id % 2 == 0"))
+
+# Upsert (merge) new data
+newData = spark.range(0, 20)
+
+deltaTable.alias("oldData") \
+  .merge(
+    newData.alias("newData"),
+    "oldData.id = newData.id") \
+  .whenMatchedUpdate(set = { "id": col("newData.id") }) \
+  .whenNotMatchedInsert(values = { "id": col("newData.id") }) \
+  .execute()
+
+deltaTable.toDF().show()
+
+
+## Read older version of data
+df = spark.read.format("delta").option("versionAsOf", 0).load("/tmp/delta-table")
+df.show()
+
+
+### Streaming
+
+streamingDf = spark.readStream.format("rate").load()
+stream = streamingDf.selectExpr("value as id").writeStream.format("delta").option("checkpointLocation", "/tmp/checkpoint").start("/tmp/delta-table")
+
+# Wait for some time, then stop the stream
+sleep(60)
+stream.stop()
